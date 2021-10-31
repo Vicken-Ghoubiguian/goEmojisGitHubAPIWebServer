615ef8ffc0ffc38d966371b167dfffdf012cd8aa
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-10-31 09:06:18 +0100

    Effects in tooltips

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,19 @@
 $(function() {
 
     //
-    $("#main_page_title").tooltip();
+    $("#main_page_title").tooltip({
+        show: {
+            effect: "slideDown",
+            delay: 250
+          }
+    });
 
     //
-    $("#main_page_subtitle").tooltip();
+    $("#main_page_subtitle").tooltip({
+        show: {
+            effect: "slideDown",
+            delay: 250
+          }
+    });
 
 });
