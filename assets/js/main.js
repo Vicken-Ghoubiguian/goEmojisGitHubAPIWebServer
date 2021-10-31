03b2a85a96113311950f6148de9e4fc52101f622
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-10-31 10:11:22 +0100

    For its 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,15 @@ $(function() {
     $("[data-toggle='tooltip']").tooltip({
         
         html: true,
+        tooltipClass: "main_tooltip-styling",
         show: {
-            effect: "slideDown",
+            effect: "explode",
             delay: 250
-      }
+        },
+        hide: {
+            effect: "explode",
+            delay: 250
+        }
     });
 
 });
